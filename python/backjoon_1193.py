0d4957f4eb51cf512f42e9ca0d89a29d0ed1f0aa
Kim Da Ye <78843974+daye9005kim@users.noreply.github.com>
2024-04-02 13:54:28 +0900

    백준 1193번 문제

--- python/backjoon_1193.py
+++ python/backjoon_1193.py
@@ -1,3 +1,24 @@
 # https://www.acmicpc.net/problem/1193
 import sys
 
+n = int(sys.stdin.readline().strip())
+sum = 0
+dd = 0
+line = 1
+
+for i in range(1, n + 1):
+    sum += i
+    dd = sum - n
+
+    if dd >= 0:
+        line = i
+        break
+
+if line % 2 < 1:
+    a = line - dd
+    b = 1 + dd
+else:
+    a = 1 + dd
+    b = line - dd
+
+print(f'{a}/{b}')
