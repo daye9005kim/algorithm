65ad4d0d25911c2a84bd9d1be083f16d60082837
Kim Da Ye <78843974+daye9005kim@users.noreply.github.com>
2024-05-09 11:37:49 +0900

    백준 24262 문제. 시간 복잡도 단일 for문

---
+++ python/backjoon_24263.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/24263
+import sys
+
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         sum <- sum + A[i]; # 코드1
+#     return sum;
+# }
+
+n = int(sys.stdin.readline().strip())
+print(n)
+print(1)
