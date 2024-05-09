1977b2ffbc52631dd078ce80c156430e7b306154
Kim Da Ye <78843974+daye9005kim@users.noreply.github.com>
2024-05-09 11:45:27 +0900

    백준 24264 문제. 시간 복잡도 중첩 for문

---
+++ python/backjoon_24264.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/24264
+import sys
+
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             sum <- sum + A[i] × A[j]; # 코드1
+#     return sum;
+# }
+
+n = int(sys.stdin.readline().strip())
+print(n * n)
+print(2)
