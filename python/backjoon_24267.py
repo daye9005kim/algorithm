6293494a85e924c6e11cd7ffe95a8824def48f05
Kim Da Ye <78843974+daye9005kim@users.noreply.github.com>
2024-05-10 16:47:40 +0900

    백준 24267 문제. 시간 복잡도 삼중첩 for문

---
+++ python/backjoon_24267.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/24267
+import sys
+
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             for k <- 1 to n
+#                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#     return sum;
+# }
+
+n = int(sys.stdin.readline().strip())
+
+print(n * (n-1) * (n-2) // 6)
+print(3)
